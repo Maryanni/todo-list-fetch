eee89962a884ffb9bf0da26c866ca60e469b62dd
Maryanni <marziliano14n@gmail.com>
2024-07-20 17:20:30 -0400

    Second change

--- src/App.js
+++ src/App.js
@@ -1,23 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [inputValue, setInputValue] = useState({
+    needToBeDone: "",
+  });
+
+  const [list, setList] = useState([]);
+
+  const handlerOnChange = (event) => {
+    setInputValue({
+      ...inputValue,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const getListCard = async () => {
+    try {
+      const response = await fetch(
+        "https://playground.4geeks.com/todo/users/maryanni",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const data = await response.json();
+      setList(data.todos);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const postCardList = async () => {
+    let bodyPost = {
+      label: inputValue.needToBeDone,
+      is_done: false,
+    };
+    console.log(bodyPost);
+    try {
+      const responsePost = await fetch(
+        "https://playground.4geeks.com/todo/todos/maryanni",
+        {
+          method: "POST",
+          body: JSON.stringify(bodyPost),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const dataPost = await responsePost.json();
+      getListCard();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getListCard();
+  }, []);
+
+  const handleOnSubmit = (event) => {
+    event.preventDefault();
+    postCardList();
+    setInputValue({ needToBeDone: "" });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App Container classContainer">
+      <div className="classDivPrincipal">
+        <h1 className="justify-content-center d-flex">Todos</h1>
+        <div className="card">
+          <div className="card-body">
+            <form onSubmit={handleOnSubmit}>
+              <input
+                onChange={handlerOnChange}
+                name="needToBeDone"
+                value={inputValue.needToBeDone}
+                className="placeholderInput"
+                type="text"
+                placeholder="What needs to be done?"
+              />
+            </form>
+          </div>
+        </div>
+        <ul className="list-group">
+          {list.map((item, index) => {
+            return (
+              <li className="list-group-item trushIcon" key={index}>
+                {item.label}
+                <span>
+                  <i className="fa fa-regular fa-rectangle-xmark"></i>
+                </span>
+              </li>
+            );
+          })}
+          <li className="list-group-item bg-secondary-subtle">
+            <b> {list.length} </b> items left
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
